2c1a69d9af90167170d2a78e96cc8e28d44a44b2
ChongWang <wangchong6806@163.com>
2026-05-19 09:33:18 +0800

    optimize house object

--- src/components/viewport/SceneObject3D.tsx
+++ src/components/viewport/SceneObject3D.tsx
@@ -55,13 +55,63 @@ function CompoundObject({ kind, color }: { kind: string; color: string }) {
     );
     case 'house': return (
       <group>
-        <mesh position={[0, 0.5, 0]}>
-          <boxGeometry args={[1, 1, 1]} />
+        {/* Main body */}
+        <mesh position={[0, 0.8, 0]}>
+          <boxGeometry args={[1.2, 1.6, 0.9]} />
           <meshStandardMaterial color={color} />
         </mesh>
-        <mesh position={[0, 1.25, 0]} rotation={[0, 0, 0]}>
-          <coneGeometry args={[0.7, 0.5, 4]} />
-          <meshStandardMaterial color={c.clone().multiplyScalar(0.6)} />
+        {/* Roof */}
+        <mesh position={[0, 1.7, 0]} rotation={[0, Math.PI / 4, 0]}>
+          <coneGeometry args={[0.9, 0.7, 4]} />
+          <meshStandardMaterial color={c.clone().multiplyScalar(0.55)} />
+        </mesh>
+        {/* Door */}
+        <mesh position={[0, 0.3, 0.46]}>
+          <boxGeometry args={[0.18, 0.6, 0.05]} />
+          <meshStandardMaterial color="#6B3A2A" />
+        </mesh>
+        {/* Door knob */}
+        <mesh position={[0.06, 0.32, 0.49]}>
+          <sphereGeometry args={[0.02, 8, 8]} />
+          <meshStandardMaterial color="#DAA520" metalness={0.8} roughness={0.3} />
+        </mesh>
+        {/* Left window */}
+        <mesh position={[-0.3, 0.85, 0.46]}>
+          <boxGeometry args={[0.22, 0.28, 0.04]} />
+          <meshStandardMaterial color="#87CEEB" emissive="#87CEEB" emissiveIntensity={0.2} />
+        </mesh>
+        {/* Left window frame - cross */}
+        <mesh position={[-0.3, 0.85, 0.48]}>
+          <boxGeometry args={[0.24, 0.03, 0.01]} />
+          <meshStandardMaterial color="#E8E0D8" />
+        </mesh>
+        <mesh position={[-0.3, 0.85, 0.48]}>
+          <boxGeometry args={[0.03, 0.30, 0.01]} />
+          <meshStandardMaterial color="#E8E0D8" />
+        </mesh>
+        {/* Right window */}
+        <mesh position={[0.3, 0.85, 0.46]}>
+          <boxGeometry args={[0.22, 0.28, 0.04]} />
+          <meshStandardMaterial color="#87CEEB" emissive="#87CEEB" emissiveIntensity={0.2} />
+        </mesh>
+        {/* Right window frame - cross */}
+        <mesh position={[0.3, 0.85, 0.48]}>
+          <boxGeometry args={[0.24, 0.03, 0.01]} />
+          <meshStandardMaterial color="#E8E0D8" />
+        </mesh>
+        <mesh position={[0.3, 0.85, 0.48]}>
+          <boxGeometry args={[0.03, 0.30, 0.01]} />
+          <meshStandardMaterial color="#E8E0D8" />
+        </mesh>
+        {/* Chimney */}
+        <mesh position={[0.3, 1.9, -0.15]}>
+          <boxGeometry args={[0.14, 0.45, 0.14]} />
+          <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        </mesh>
+        {/* Roof ridge */}
+        <mesh position={[0, 2.05, 0]}>
+          <boxGeometry args={[0.06, 0.08, 1.1]} />
+          <meshStandardMaterial color={c.clone().multiplyScalar(0.45)} />
         </mesh>
       </group>
     );
